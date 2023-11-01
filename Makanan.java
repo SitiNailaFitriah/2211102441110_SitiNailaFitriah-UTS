af4999ef62f7be8bd1afedfa8f942f634adf6b4b
SitiNailaFitriah <nailafitriah54@gmail.com>
2023-11-01 13:46:28 +0800

    first commit

---
+++ Makanan.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  
+
+public class Makanan extends Actor
+{
+    public void addedToWorld(World Latar)
+    {
+        
+    }
+    
+    
+    
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
